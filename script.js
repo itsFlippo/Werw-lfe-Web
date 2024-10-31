9f623692df1442ffc684d55938d13d2a5039d189
Flippo <79870687+itsFlippo@users.noreply.github.com>
2024-10-31 16:40:13 +0100

    Biggest update so far!
    - new cards
    - new overall look
    - new button to play around with

--- script.js
+++ script.js
@@ -1,42 +1,54 @@
 // Hintergrundmusik und Variablen
-let backgroundMusic = new Audio("Hintergrundmusik/hintergrund.mp3"); // Pfad zur Hintergrundmusik
+let backgroundMusic = new Audio("Hintergrundmusik/hintergrund.mp3");
 backgroundMusic.loop = true;
-backgroundMusic.volume = 0.6; // Lautstärke auf 50%
+backgroundMusic.volume = 0.5; // Lautstärke auf 50%
+
+// Standard-Zeit für "gong" in Sekunden (10 Minuten)
+let gongDelay = 600;
 
 // Reihenfolge der Zeiten und Rollen in der Abspielreihenfolge
 const audioSequence = [
-    { audio: "start", roles: [], alwaysPlay: true }, // 1
-    { audio: null, roles: ["schutzengel"], condition: () => hasRole(["schutzengel"]) }, // 2
-    { audio: null, roles: ["nachahmer"], condition: () => hasRole(["nachahmer"]) }, // 3
-    { audio: "daemmerung", roles: [], condition: () => hasRole(["der-graf", "renfield", "infizierter", "venus", "anstifterin", "priester", "meuchler", "verliebte"]) }, // 4
-    { audio: "vampire", roles: [], condition: () => hasRole(["der-meister", "der-graf", "vampir"]) }, // 5
-    { audio: null, roles: ["der-graf", "renfield", "infizierter", "venus", "anstifterin", "priester", "meuchler"], condition: () => hasRole(["der-graf", "renfield", "infizierter", "venus", "anstifterin", "priester", "meuchler"]) }, // 6-12
-    { audio: "marken", roles: [], condition: () => hasRole(["der-graf", "renfield", "infizierter", "venus", "anstifterin", "priester", "meuchler"]) }, // 13
-    { audio: null, roles: ["verliebte"], condition: () => hasRole(["verliebte"]) }, // 14
-    { audio: "hoellenzeit", roles: [], condition: () => hasRole(["knochenleser_1", "hoellenfuerst", "flammenbeschwoerer", "seelendieb", "schattenlaeufer_1", "todesengel", "knochenleser_2"]) }, // 15
-    { audio: "daemonen", roles: [], condition: () => hasRole(["hoellenfuerst", "flammenbeschwoerer", "seelendieb", "schattenlaeufer"]) }, // 16
-    { audio: null, roles: ["knochenleser_1", "hoellenfuerst", "flammenbeschwoerer", "seelendieb", "schattenlaeufer_1", "todesengel", "knochenleser_2"], condition: () => hasRole(["knochenleser_1", "hoellenfuerst", "flammenbeschwoerer", "seelendieb", "schattenlaeufer", "todesengel", "knochenleser_2"]) }, // 17-23
-    { audio: "nacht", roles: [], alwaysPlay: true }, // 24
-    { audio: null, roles: ["wache"], condition: () => hasRole(["wache"]) }, // 25
-    { audio: "werwoelfe", roles: [], condition: () => hasRole(["alphawolf", "sehender-wolf", "traumwolf", "werwolf"]) }, // 26
+    { audio: "start", roles: [], alwaysPlay: true },
+    { audio: null, roles: ["schutzengel"], condition: () => hasRole(["schutzengel"]) },
+    { audio: null, roles: ["nachahmer"], condition: () => hasRole(["nachahmer"]) },
+    { audio: "daemmerung", roles: [], condition: () => hasRole(["der-graf", "renfield", "infizierter", "venus", "anstifterin", "priester", "meuchler", "verliebte"]) },
+    { audio: "vampire", roles: [], condition: () => hasRole(["der-meister", "der-graf", "vampir"]) },
+    { audio: null, roles: ["der-graf", "renfield", "infizierter", "venus", "anstifterin", "priester", "meuchler"], condition: () => hasRole(["der-graf", "renfield", "infizierter", "venus", "anstifterin", "priester", "meuchler"]) },
+    { audio: "marken", roles: [], condition: () => hasRole(["der-graf", "renfield", "infizierter", "venus", "anstifterin", "priester", "meuchler"]) },
+    { audio: null, roles: ["verliebte"], condition: () => hasRole(["verliebte"]) },
+    { audio: "hoellenzeit", roles: [], condition: () => hasRole(["knochenleser_1", "hoellenfuerst", "flammenbeschwoerer", "seelendieb", "schattenlaeufer_1", "todesengel", "knochenleser_2"]) },
+    { audio: "daemonen", roles: [], condition: () => hasRole(["hoellenfuerst", "flammenbeschwoerer", "seelendieb", "schattenlaeufer"]) },
+    { audio: null, roles: ["knochenleser_1", "hoellenfuerst", "flammenbeschwoerer", "seelendieb", "schattenlaeufer_1", "todesengel", "knochenleser_2"], condition: () => hasRole(["knochenleser_1", "hoellenfuerst", "flammenbeschwoerer", "seelendieb", "schattenlaeufer", "todesengel", "knochenleser_2"]) },
+    { audio: "nacht", roles: [], alwaysPlay: true },
+    { audio: null, roles: ["wache"], condition: () => hasRole(["wache"]) },
+    { audio: "werwoelfe", roles: [], condition: () => hasRole(["alphawolf", "sehender-wolf", "traumwolf", "werwolf"]) },
     { audio: null, roles: [
         "alphawolf", "sehender-wolf", "traumwolf", "das-ding", "die-unsichtbare", "nebelseher",
-        "flammenwaechter", "daemonenjaeger", "seherin", "junger-seher", "die-erleuchtete", "rufer-der-toten",
+        "flammenwaechter", "daemonenjaeger", "seherin", "junger-seher", "die-erleuchtete", "weise-eule", "kobold", "rufer-der-toten",
         "geisterjaeger", "spurenleser", "glockenlaeuter", "raeuber", "exorzist", "hexe", "lichtbringer",
         "schattenwandler", "taschendieb", "unruhestifterin", "dorftrottel", "magier-des-chaos", "geistermedium",
-        "sternendeuterin", "wahrsagerin", "kobold", "weise-eule", "der-prophet", "betrunkener", "schlaflose",
+        "sternendeuterin", "wahrsagerin", "der-prophet", "betrunkener", "schlaflose",
         "der-glasmacher", "aufklaerer", "der-schicksalsweber", "kurator", "spielsuechtiger", "sensenmann",
         "schattenlaeufer_2"
     ], condition: () => hasRole([
         "alphawolf", "sehender-wolf", "traumwolf", "das-ding", "die-unsichtbare", "nebelseher",
-        "flammenwaechter", "daemonenjaeger", "seherin", "junger-seher", "die-erleuchtete", "rufer-der-toten",
+        "flammenwaechter", "daemonenjaeger", "seherin", "junger-seher", "die-erleuchtete", "weise-eule", "kobold", "rufer-der-toten",
         "geisterjaeger", "spurenleser", "glockenlaeuter", "raeuber", "exorzist", "hexe", "lichtbringer",
         "schattenwandler", "taschendieb", "unruhestifterin", "dorftrottel", "magier-des-chaos", "geistermedium",
-        "sternendeuterin", "wahrsagerin", "kobold", "weise-eule", "der-prophet", "betrunkener", "schlaflose",
+        "sternendeuterin", "wahrsagerin", "der-prophet", "betrunkener", "schlaflose",
         "der-glasmacher", "aufklaerer", "der-schicksalsweber", "kurator", "spielsuechtiger", "sensenmann",
         "schattenlaeufer_2"
-    ]) }, // 27-77
-    { audio: "ende", roles: [], alwaysPlay: true } // 78
+    ]) },
+    { audio: "ende", roles: [], alwaysPlay: true },
+    {
+        audio: "abstimmung",
+        roles: [],
+        alwaysPlay: true,
+        nextAudio: {
+            audio: "gong",
+            delay: gongDelay // Anpassbare Zeit in Sekunden
+        }
+    }
 ];
 
 // Funktion zum Überprüfen, ob mindestens eine der Rollen ausgewählt ist
@@ -52,12 +64,21 @@ const startStopButton = document.getElementById("start-stop-button");
 let isRunning = false; // Speichert den aktuellen Zustand (Start oder Stop)
 let currentAudio; // Variable für die aktuelle Audiosequenz, falls gestoppt werden soll
 
+// Event-Listener für Zeitwahl-Buttons
+document.querySelectorAll(".time-button").forEach(button => {
+    button.addEventListener("click", function () {
+        document.querySelectorAll(".time-button").forEach(btn => btn.classList.remove("selected"));
+        this.classList.add("selected");
+        gongDelay = parseInt(this.getAttribute("data-time")); // Setzt die Verzögerungszeit
+    });
+});
+
 // Start-Stop-Funktionalität
 startStopButton.addEventListener("click", function () {
     if (isRunning) {
-        stopGame(); // Wenn "Stop" geklickt wird
+        stopGame();
     } else {
-        startGame(); // Wenn "Start" geklickt wird
+        startGame();
     }
 });
 
@@ -100,22 +121,18 @@ function playAudioSequence(sequence) {
     let index = 0;
 
     function playNext() {
-        if (!isRunning) return; // Abbruch, wenn gestoppt
+        if (!isRunning) return;
 
         if (index >= sequence.length) {
-            console.log("Sequenz abgeschlossen, Hintergrundmusik wird gestoppt.");
-            stopBackgroundMusic();
-            stopGame(); // Setzt den Button zurück
+            stopGame();
             return;
         }
 
-        const { audio, roles, condition, alwaysPlay } = sequence[index];
-        index++; // Bereitet den nächsten Durchlauf vor, bevor die Bedingungen geprüft werden
+        const { audio, roles, condition, alwaysPlay, nextAudio } = sequence[index];
+        index++;
 
-        // Nur abspielen, wenn immer abspielbar oder die Bedingung erfüllt ist
         if (alwaysPlay || (condition && condition())) {
             if (audio) {
-                console.log(`Spiele Audio: ${audio}`);
                 currentAudio = new Audio(`Rollenaudio/${audio}.mp3`);
                 currentAudio.play();
                 currentAudio.onended = () => {
@@ -125,11 +142,18 @@ function playAudioSequence(sequence) {
                         playNext();
                     }
                 };
+
+                // "gong" Delay aktualisieren, wenn "abstimmung" Audio spielt
+                if (audio === "abstimmung" && nextAudio && nextAudio.audio === "gong") {
+                    setTimeout(() => {
+                        const gongAudio = new Audio(`Rollenaudio/${nextAudio.audio}.mp3`);
+                        gongAudio.play();
+                    }, gongDelay * 1000);
+                }
             } else {
                 playRoleSequence(roles, playNext);
             }
         } else {
-            // Bedingung nicht erfüllt, überspringe zur nächsten Audio
             playNext();
         }
     }
@@ -142,7 +166,7 @@ function playRoleSequence(roles, callback) {
     let roleIndex = 0;
 
     function playNextRole() {
-        if (!isRunning) return; // Abbruch, wenn gestoppt
+        if (!isRunning) return;
 
         if (roleIndex >= roles.length) {
             callback();
@@ -150,45 +174,18 @@ function playRoleSequence(roles, callback) {
         }
 
         const role = roles[roleIndex];
-        roleIndex++; // Erhöht vor dem Überprüfen, damit übersprungen wird, falls Bedingung nicht erfüllt ist
+        roleIndex++;
 
-        // Checkbox-Element für die aktuelle Rolle suchen
         const checkbox = document.querySelector(`input[type='checkbox'][value='${role}']`);
 
         if (checkbox && checkbox.checked) {
-            console.log(`Spiele Rolle: ${role}`);
             currentAudio = new Audio(`Rollenaudio/${role}.mp3`);
             currentAudio.play();
             currentAudio.onended = playNextRole;
         } else {
-            // Rolle nicht ausgewählt, überspringe zur nächsten Rolle
             playNextRole();
         }
     }
 
     playNextRole();
 }
-
-// Event-Listener für die Haupt-Checkbox "Knochenleser"
-document.getElementById("knochenleser_1").addEventListener("change", function() {
-    const secondCheckbox = document.getElementById("knochenleser_2");
-
-    // Wenn die erste Checkbox ausgewählt ist, aktiviere die zweite
-    if (this.checked) {
-        secondCheckbox.checked = true; // Aktiviere die zweite Checkbox im Hintergrund
-    } else {
-        secondCheckbox.checked = false; // Deaktiviere die zweite Checkbox im Hintergrund
-    }
-});
-
-// Event-Listener für die Haupt-Checkbox "Schattenläufer"
-document.getElementById("schattenlaeufer_1").addEventListener("change", function() {
-    const secondCheckbox = document.getElementById("schattenlaeufer_2");
-
-    // Wenn die erste Checkbox ausgewählt ist, aktiviere die zweite
-    if (this.checked) {
-        secondCheckbox.checked = true; // Aktiviere die zweite Checkbox im Hintergrund
-    } else {
-        secondCheckbox.checked = false; // Deaktiviere die zweite Checkbox im Hintergrund
-    }
-});
